93b2715bd998eb3f6d74ac14ceb442d76ffa382a
Gowri Addepalli <gauriaddepalli@gmail.com>
2023-06-17 17:44:03 -0400

    Update LC_692_topKFrequent.py

--- LC_692_topKFrequent.py
+++ LC_692_topKFrequent.py
@@ -10,3 +10,21 @@ class Solution(object):
     
         count = Counter(words)
         return sorted(list(count.keys()), key = lambda x: (-count[x], x)) [:k]
+    
+    
+ from collections import Counter
+from heapq import heapify, heappop
+
+class Solution(object):
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+    
+        count = Counter(words)
+        heap = [(-freq, word) for word,freq in count.items()]
+        heapify(heap)
+
+        return [heappop(heap)[1] for _ in range(k)]
